ab8d60880fc930df2a3f58a5c94dfbea711ce29a
MonicaAmaya13 <115158073+MonicaAmaya13@users.noreply.github.com>
2023-03-23 14:43:12 +0100

    Add files via upload

--- TODOSEJERCICIOS.js
+++ TODOSEJERCICIOS.js
@@ -26,5 +26,57 @@ console.log("El total area de radio 30 es "+ total_area);
 // LOS DOS NÚMEROS 
 
 function mayor(X1,X2){
-    return X1 > X2 
+    if(X1>X2){
+        return X1>X2
+
+    }
 }
+
+//5. CREA UNA FUNCIÓN QUE TOME DOS NÚMEROS COMO ARGUMENTOS Y DEVUELVA EL RESULTADO 
+// DE DIVIDIR EL PRIMER NÚMERO POR EL SEGUNDO NÚMERO. ASEGÚRATE DE MANEJAR 
+// EL CASO EN EL QUE EL SEGUNDO NÚMERO SEA CERO 
+
+function DIVISIÓN ARGUMENTOS(X1:X2){
+    if (b==0){
+        console.log("No se puede divir entre 0");
+    }else{
+        return X1/X2;
+    }
+
+}
+const RESULTADO = dividir(64,2);
+console.log("El resultado de la división es 6")
+
+
+
+//6. CREA UNA FUNCION QUE TOME UN NUMERO COMO ARGUMENTO Y DELVUELVA UNA CADENA QUE INDIQUE 
+//SI EL NUMERO ES PAR O IMPAR 
+
+function par_impar(X){
+    if (x%2==0){
+            return "par";
+    }else{
+                 
+        return "impar"; 
+    }
+}
+console.log(par_impar(4));
+
+//7. FUNCIÓN QUE TOME LA EDAD DE UNA PERSONA COMO ARGUMENTO Y DEVUELVA 
+//LA CLASIFICACIÓN ETARIA APROPIADA 
+
+function CLASIFICACIÓN(EDAD){
+    if(edad<13){
+        return "niño";
+    }else if(13<EDAD && EDAD<18){
+        return "ADOLECENTE";
+    }else if(19<EDAD && EDAD<64){
+        return "ADULTO";
+    }else if(edad>64){
+        return "ANCIANO";
+    }
+}
+
+    console.log("12 --> " + clasificacion(12)); 
+
+
---
+++ clasificacion.js
@@ -0,0 +1,13 @@
+//8.Escribe una función que tome una calificación numérica (0-100) como argumento 
+//y devuelva la calificación en letras correspondiente:
+function clasificar_num(a) {
+    if (a>90) {
+        return 'a';
+    }else{if  (a>80 && a<89) {
+        return 'b';
+    }else{if 
+
+    }
+
+    }
+}
